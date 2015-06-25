c0deeb83736de10acfdf416a588c435a46c1f574
unknown <p0905121@IUTDOUAB2PC5.univ-lyon1.fr>
2015-06-25 09:37:07 +0200

    True test DéfinirUneClasseRealisantUneMultiplication

---
+++ FinalTest/Multiplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FinalTest.Tests
+{
+    public class Multiplication
+    {
+        public Boolean PeutCalculer(string s1)
+        {
+            var r = s1.Split('*');
+            var result = r.Length > 1;
+            return result;
+
+        }
+
+        public int Calculer(string s)
+        {
+            var donne = s.Split('*');
+            var result = Convert.ToInt32(donne[0]) * Convert.ToInt32(donne[1]);
+            return result;
+
+        }
+    }
+}

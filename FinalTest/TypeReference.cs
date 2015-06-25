d9a93908c99a136cb10b1f2f5af7d778fc9dd2a3
unknown <p0905121@IUTDOUAB2PC5.univ-lyon1.fr>
2015-06-25 09:21:38 +0200

    test DéfinirUnTypeRéférenceAvecEgalitéDeuxInstancesAyantLesMêmesPropriétés ok

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FinalTest.Tests
+{
+    public class TypeReference
+    {
+        private readonly int _i;
+        private object o;
+
+        public TypeReference(int i)
+        {
+            _i = i;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != this.GetType()) return false;
+            TypeReference other = (TypeReference) obj;
+            return other.o == this.o;
+
+
+        }
+    }
+
+}

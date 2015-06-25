4b98df06d1365268d2ab3386be0243a8ddb627bf
unknown <p0905121@IUTDOUAB2PC5.univ-lyon1.fr>
2015-06-25 10:37:07 +0200

    test Déf_keyValuePairs TRUE

---
+++ FinalTest/NewFolder1/Nombres.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FinalTest.Tests
+{
+    public class Nombres
+    {
+        private readonly IEnumerable<KeyValuePair<string, int>> _keyValuePairs;
+
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            _keyValuePairs = keyValuePairs;
+        }
+
+        public IEnumerable<int> NombresPairs
+        {
+            get
+            {
+                var requete = _keyValuePairs.Where(nombre => nombre.Value % 2 ==0).Select(nombre => nombre.Value);
+                return requete;
+            }
+        } 
+    }
+}
